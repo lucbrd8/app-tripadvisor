50905f3316c14ede2b317fdf44dd73295c446c5a
Valentine Sterlin <valentine.sterlin@etu.minesparis.psl.eu>
2025-06-30 10:31:14 +0200

    modif val

---
+++ app/index2.tsx
@@ -0,0 +1,21 @@
+import { Text, View,  StyleSheet } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e', //changer la couleur du fond
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff', //changer la couleur du texte
+  },
+});
