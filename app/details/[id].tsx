9a9818c4c6f1c75e8d4dc16de3443256daed107d
Luc <lucbiard57@gmail.com>
2025-07-03 14:06:02 +0200

    dernière version

--- app/details/[id].tsx
+++ app/details/[id].tsx
@@ -1,59 +1,93 @@
 // app/details/[id].tsx
-import { useLocalSearchParams } from 'expo-router';
-import { View, Text, StyleSheet } from 'react-native';
-import { Stack } from 'expo-router';
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function DetailScreen() {
-  const { id, label, globalRating, cleanRating, location, isShower, staffRating, showerRate } = useLocalSearchParams();
-  
-  let shower: string = 'Non spécifié';
+  const {
+    id,
+    label,
+    globalRating,
+    cleanRating,
+    location,
+    isShower,
+    staffRating,
+    showerRate,
+  } = useLocalSearchParams();
+  const router = useRouter();
 
-  if (isShower == 'false') {
-    shower = 'Yes';
+  let shower: string = "Non spécifié";
+
+  if (isShower == "false") {
+    shower = "Yes";
   } else {
-    shower = 'No';
+    shower = "No";
   }
-    
 
   return (
     <View style={styles.container}>
       <Stack.Screen
-      options={{
-        title: 'Détails',
-        headerStyle: {
-          backgroundColor: '#483C32',
-        },
-        headerTintColor: '#B9A896',
-        headerTitleStyle: { fontWeight: 'bold' },
-      }}
-    />
+        options={{
+          title: "Détails",
+          headerStyle: {
+            backgroundColor: "#483C32",
+          },
+          headerTintColor: "#B9A896",
+          headerTitleStyle: { fontWeight: "bold" },
+        }}
+      />
       <Text style={styles.text}>Name : {label}</Text>
       <Text style={styles.text}>Global Rate : {globalRating}</Text>
       <Text style={styles.text}>Localisation : {location}</Text>
-      <Text style={styles.text}>Staff Rate : {staffRating}</Text> 
+      <Text style={styles.text}>Staff Rate : {staffRating}</Text>
       <Text style={styles.text}>Cleaning Rate : {cleanRating}</Text>
       <Text style={styles.text}>Shower ? : {shower}</Text>
       <Text style={styles.text}>Shower Rate : {showerRate}</Text>
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={() =>
+          router.push({
+            pathname: "../(list-subtabs)/addReview[id]",
+            params: { id, label },
+          })
+        }
+      >
+        <Text style={styles.buttonText}>Add a review on the rest area</Text>
+      </TouchableOpacity>
     </View>
-    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     padding: 24,
-    backgroundColor: '#B9A896',
+    backgroundColor: "#B9A896",
     flex: 1,
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 12,
-    color: '#483C32',
+    color: "#483C32",
   },
   text: {
     fontSize: 18,
     marginBottom: 8,
-    color: '#483C32',
+    color: "#483C32",
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 30,
+    alignSelf: "center",
+    borderRadius: 10,
+    backgroundColor: "#65a765",
+    padding: 15,
+    height: 50,
+    width: 200,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
   },
 });
